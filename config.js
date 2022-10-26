61976a344e234941a3be88480890bd6506fd1b03
Cam A <camran.ahmad@gmail.com>
2022-10-26 12:47:07 -0700

    time of day to add instruction

--- config.js
+++ config.js
@@ -1,3 +1,13 @@
+/*
+*   configurable values for app
+*   calendarId = Google Calendar ID obtained from calendar integration settings
+*   myKey = Google API key obtained from google cloud console https://console.cloud.google.com/
+*   cardHeight = height of each calendar card
+*   transitionSpeed = How quickly the cards scroll
+*   dateOffset = Number of days to obtain from calendar
+*   resetTime = time of day in 24h clock to get new data from API
+*/
+
 let configuration = {
     calendarId: "wttest503@gmail.com",
     myKey: "AIzaSyAszRQd8o1G8zXgu45v5UyrXVTFKUwySRk",
