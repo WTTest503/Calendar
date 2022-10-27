c5543295a02f23f5ec9f0a15c2efbe8fbe2e4702
Cam A <camran.ahmad@gmail.com>
2022-10-27 12:07:25 -0700

    update styling

--- app.js
+++ app.js
@@ -11,43 +11,25 @@ async function getData (eStart, eEnd) {
         activeEvents = activeEvents.filter(event => event.status != 'cancelled').sort((a, b) => new Date(a.start.dateTime) - new Date(b.start.dateTime))
         sortedEvents = groupDates(activeEvents)
         
-
         for (evt in sortedEvents) {
-            let eventDate = new Date(evt)
-            eventDate = `${eventDate.toLocaleString('default', { month: 'long' })} ${eventDate.getDate()}`
-            console.log(eventDate)
-            console.log(sortedEvents[evt])
+            
+            let eventDate = new Date(`${evt}T00:00:00`)
+            eventDateMonth = `${eventDate.toLocaleString('default', { month: 'long' })}`
+            eventDateDay = `${eventDate.getDate()}`
             let innerList = ''
             for (subEvt in sortedEvents[evt]) {
-                console.log(sortedEvents[evt][subEvt])
                 let title = sortedEvents[evt][subEvt].summary
                 let eventStart = new Date(sortedEvents[evt][subEvt].start.dateTime)
                 let eventEnd = new Date(sortedEvents[evt][subEvt].end.dateTime)
-                let eventStartTime = eventStart.toLocaleTimeString()
-                let eventEndTime = eventEnd.toLocaleTimeString()
-                innerList += `<li><p class="cardTitle">${title}</p>  <p class="eventTime">${eventStartTime} - ${eventEndTime}</p></li>`
+                let eventStartTime = eventStart.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+                let eventEndTime = eventEnd.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})
+                innerList += `<li class="theEvents"><p class="cardTitle">${title}</p>  <p class="eventTime">${eventStartTime}</p></li>`
             }
                 
-            document.getElementById('theBase').innerHTML += `<div style="height:${cardHeight}px;" class='scrollCard'><div class="grid1"><p class="eventDate">${eventDate}</p></div><div class="grid2"><ul class="eventList">${innerList}</ul></div></div>`
+            document.getElementById('theBase').innerHTML += `<div style="height:${cardHeight}px;" class='scrollCard'><div class="dateSection"><p class="eventDate">${eventDateMonth}</p><p class="eventDate">${eventDateDay}</p></div><div><ul class="eventList">${innerList}</ul></div></div>`
 
         }
 
-        /*for (evnt in activeEvents) {
-            let title = activeEvents[evnt].summary
-            if(activeEvents[evnt].start.hasOwnProperty('dateTime')) {
-                let eventStart = new Date(activeEvents[evnt].start.dateTime)
-                let eventEnd = new Date(activeEvents[evnt].end.dateTime)
-                let eventStartTime = eventStart.toLocaleTimeString()
-                let eventEndTime = eventEnd.toLocaleTimeString()
-                let eventMonth = eventStart.toLocaleString('default', { month: 'long' })
-                let eventDay = eventStart.getDate()
-                document.getElementById('theBase').innerHTML += `<div style="height:${cardHeight}px;" class='scrollCard'><p class="eventDate">${eventMonth} ${eventDay}</p><p class="cardTitle">${title}</p><p class="eventTime">${eventStartTime} - ${eventEndTime}</p></div>`
-            }
-            else {
-                document.getElementById('theBase').innerHTML += `<div style="height:${cardHeight}px;" class='scrollCard'><p class="eventDate">${eventMonth} ${eventDay}</p><p class="cardTitle">${title}</p></div>`
-    
-            }
-        }*/
     } catch (error) {
         console.log(error)
     }
