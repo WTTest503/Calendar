9076ae1d6fea16a745ef34b5a6eed7c09282a737
Cam A <camran.ahmad@gmail.com>
2022-11-05 10:21:02 -0700

    faster scroll, larger event text

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ let configuration = {
     calendarId: "wttest503@gmail.com",
     myKey: "AIzaSyAszRQd8o1G8zXgu45v5UyrXVTFKUwySRk",
     cardHeight: 200,
-    transitionSpeed: .2,
+    transitionSpeed: .3,
     dateOffset: 30,
     resetTime: 4
 }
