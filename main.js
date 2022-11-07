de149457f5bd36caab4086c0d78080ab4f6d9e40
Cam A <camran.ahmad@gmail.com>
2022-11-07 13:25:18 -0800

    add electron build

---
+++ main.js
@@ -0,0 +1,36 @@
+const {app, BrowserWindow, Menu} = require('electron')
+  const path = require('path')
+  const url = require('url')
+  
+  const template = [
+    {
+      label: "Toggle Full Screen",
+      visible: false,
+      accelerator: "Esc",
+      click(item, focusedWindow) {
+          /*if (focusedWindow.isFullScreen()) {
+              focusedWindow.setFullScreen(false)
+          }
+          else {
+              focusedWindow.setFullScreen(true)
+          }*/
+          app.quit()
+      },
+  }
+    
+ ]
+
+  function createWindow () {
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600, fullscreen:true, autoHideMenuBar:true})
+
+    // and load the index.html of the app.
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  }
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+  app.on('ready', createWindow)
---
+++ readme.md
@@ -0,0 +1,12 @@
+### electron commands
+
+add electron-packager to cli
+```
+npm install electron-packager -g
+```
+
+Build app
+```
+electron-packager . wtCalendar --platform=win32 --arch=x64 --overwrite
+```
+
